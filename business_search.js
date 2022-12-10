e96aca382d06552af6360c3a1fe35d09a1f0c3f5
Angaye Seimokumo <angayejay@gmail.com>
2022-12-10 15:53:05 -0800

    update

--- business_search.js
+++ business_search.js
@@ -1,8 +1,10 @@
 var pick="";
 var second = "";
 var first = "";
+var locsearch="";
 function activeSearch(){
   pick=document.getElementById("catselect").value;
+  locsearch=document.getElementById("locselect").value;
   store();
 
 if (pick=="Boutique"){
@@ -19,18 +21,19 @@ function store () {
   // (A) VARIABLES TO PASS
  
 first= pick;
-second = pick+" second one";
+second =locsearch;
 
   // (B) SAVE TO SESSION STORAGE
   // (B1) FLAT STRING OR NUMBER
   // SESSIONSTORAGE.SETITEM("KEY", "VALUE");
   sessionStorage.setItem("first", first);
-
+  sessionStorage.setItem("second", second);
   // (B2) ARRAY OR OBJECT
   // SESSION STORAGE CANNOT STORE ARRAY AND OBJECTS
   // JSON ENCODE BEFORE STORING, CONVERT TO STRING
   sessionStorage.setItem("second", JSON.stringify(first));
-
+sessionStorage.setItem("second", JSON.stringify(second));
+ 
   // (C) REDIRECT
   location.href = "search.html";
   // window.open("1b-session.html");
